80844d4742396467d2dd2baa6e875c4f4c6c49d4
Tom Deering <tdeering@iastate.edu>
2015-03-03 09:30:53 -0600

    Commit dex2jar as tooling, updates to gatherjars script.

--- Tools/gatherjars.sh
+++ Tools/gatherjars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # A script which finds the JARs that Android built, 
-# converts those JARS from DEX to CLASS bytecode
-# extracts the JARS into a unified folder structure
-# combines them into one super jar
+# converts those JARS from DEX to CLASS bytecode. 
+# You can then add them to the build path of a project
+# and generate Jimple.
 #
 # The goal is to be able to get Jimple from DEX that Android built and installed for the platform.
 
@@ -13,11 +13,10 @@ DEX_DIR=/home/tdeering/android/out/dex
 # Directory for output of JARs of class files
 CLASS_DIR=/home/tdeering/android/out/jar
 # dex2jar script
-DEX2JAR="/home/tdeering/Downloads/dex2jar-0.0.9.15/d2j-dex2jar.sh"
+DEX2JAR=/home/tdeering/git/AnalyzableAndroid/Tools/dex2jar-0.0.9.15/d2j-dex2jar.sh
 
 mkdir "$DEX_DIR"
-mkdir "$CLASS_JAR_DIR"
-JAR=".jar"
+mkdir "$CLASS_DIR"
 
 for DEX_JAR in `find $FRAMEWORK_DIR -type f -name "*.jar"`
 do
@@ -31,16 +30,3 @@ unzip -p $DEX_JAR "classes.dex" >"$DEX_DIR/$NAME.dex"
 sh $DEX2JAR "$DEX_DIR/$NAME.dex" -f -o "$CLASS_DIR/$NAME.jar"
 
 done
-
-cd $CLASS_DIR
-mkdir tmp
-cd tmp
-
-for CLASS_JAR in `find $CLASS_DIR -type f -name "*.jar"`
-do
-# Extract class files into this directory
-jar -xvf $CLASS_JAR
-done
-
-cd $CLASS_DIR
-jar -cvf combined.jar -C tmp .
